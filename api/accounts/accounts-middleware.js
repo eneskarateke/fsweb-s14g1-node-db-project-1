1c1a01f012e2dd2bed39f1211aca771d4e00dfb2
eneskarateke <eneskarateke@gmail.com>
2023-06-18 21:47:52 +0300

    budget error messageta hataya neden olan undefined durumu düzeltildi

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -6,21 +6,19 @@ exports.checkAccountPayload = (req, res, next) => {
   try {
     let { name, budget } = req.body;
 
-    if (!name || !budget) {
-      return res.status(400).json({ message: "name and budget are required" });
+    if (name === undefined || budget === undefined) {
+      res.status(400).json({ message: "name and budget are required" });
     } else {
       let trimmedName = name.trim();
 
       if (trimmedName.length < 3 || trimmedName.length > 100) {
-        return res
+        res
           .status(400)
           .json({ message: "name of account must be between 3 and 100" });
       } else if (typeof budget !== "number") {
-        return res
-          .status(400)
-          .json({ message: "budget of account must be a number" });
+        res.status(400).json({ message: "budget of account must be a number" });
       } else if (budget < 0 || budget > 1000000) {
-        return res
+        res
           .status(400)
           .json({ message: "budget of account is too large or too small" });
       }
